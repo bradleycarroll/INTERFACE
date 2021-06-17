1fe4350a6943d53edc9b49221b4108170fa69faa
bradley carroll <bradleycarroll@hotmail.com>
2021-06-17 16:46:15 +0200

    ready for udates to Edit page still need drop down for apps in Create page

--- Data/Migrations/20210617121658_second.Designer.cs
+++ Data/Migrations/20210617121658_second.Designer.cs
@@ -4,14 +4,16 @@ using INTERFACE.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace INTERFACE.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210617121658_second")]
+    partial class second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
