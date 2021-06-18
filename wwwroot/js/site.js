ec9a22dd2e73a9baa4e23a9727b56864379d3fad
bradley carroll <bradleycarroll@hotmail.com>
2021-06-18 12:00:48 +0200

    JS App list in create page finished

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,13 +5,25 @@
 
 
 // the get date for entry into Logtime in Create page
-
-var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var yyyy = today.getFullYear();
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+let yyyy = today.getFullYear();
 
 today = mm + '/' + dd + '/' + yyyy;
 
+document.getElementById("myInputID").value = today; //set value on myInputID
 
-document.getElementById("myInputID").value = today ; //set value on myInputID
+
+
+// auto create drop down from array for App selection
+let select = document.getElementById("select"),
+    arr= ["APPLID","BRIDGE","COMMENTS","FINANCE_CONSO","FINANCE_INPUT","HFL","INSURANCE","INTERCO_MATCH","PEOPLEMGT","REQMOD","SALES_GP","SPECACBS","SPECLEG","SPECOPER","SUSTAIN"];
+
+for (let i = 0; i < arr.length; i++)
+{
+    let option = document.createElement("OPTION"),
+        txt = document.createTextNode(arr[i]);
+    option.appendChild(txt);
+    select.insertBefore(option, select.lastChild);
+}
